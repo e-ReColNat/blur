639e3926c1fd9c1d4efa46c40b02639bb5d01b50
eparisot <eparisot@student.42.fr>
2019-09-01 12:44:21 +0200

    jsons req / resp

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import request, abort
+from flask import request, abort, jsonify
 from flask_api import FlaskAPI, status
 from functools import wraps
 from urllib.parse import urlparse
@@ -14,29 +14,33 @@ def require_appkey(view_function):
     # the new, post-decoration function. Note *args and **kwargs here.
     def decorated_function(*args, **kwargs):
         ip = request.remote_addr
-        if request.args.get('key') and \
-           request.args.get('key') in APPKEYS and \
-           ip == APPKEYS[request.args.get('key')]:
+        key = request.headers.get("Key")
+        if key and \
+           key in APPKEYS and \
+           ip == APPKEYS[key]:
             return view_function(*args, **kwargs)
         else:
-            abort(401)
+            return jsonify({"message": "ERROR"}), status.HTTP_401_UNAUTHORIZED
     return decorated_function
 
 # Define route to API
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/api/", methods=["GET", "POST"])
 @require_appkey
 def handle_requests():
-    if request.method == 'POST':
-        req = str(request.data.get('text', ''))
-        # Process POST request and send response
+    if request.method == "POST":
+        req = str(request.data.get("Data"))
+        # Process POST request
         if len(req):
-            return req, status.HTTP_200_OK
+            return jsonify({"message": "OK"}), status.HTTP_200_OK
         else:
-            return req, status.HTTP_204_NO_CONTENT
+            return jsonify({"message": "ERROR"}), status.HTTP_204_NO_CONTENT
     else:
+        req = str(request.data.get("Data"))
         # Process GET requests
-        req = {'request data': request.data}
-        return req
+        if len(req):
+            return jsonify({"message": "OK"}), status.HTTP_200_OK
+        else:
+            return jsonify({"message": "ERROR"}), status.HTTP_204_NO_CONTENT
 
 if __name__ == "__main__":
     # Build app
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,100 @@
 import pytest
-
+import json
 import api
 
 @pytest.fixture
 def client():
-    api.app.config['TESTING'] = True
+    api.app.config["TESTING"] = True
     client = api.app.test_client()
     yield client
 
+mimetype = "application/json"
+
 # define tests bellow:
+
+# GET
 def test_get_no_auth(client):
-    response = client.get('/')
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype,
+        "Key": ""
+    }
+    data = {
+        "Data": "test"
+    }
+    url = "/api/"
+    response = client.get(url, data=json.dumps(data), headers=headers)
+    assert response.content_type == mimetype
     assert response.status_code == 401
 
 def test_get_auth(client):
-    response = client.get('/?key=TEST_KEY')
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype,
+        "Key": "TEST_KEY"
+    }
+    data = {
+        "Data": "test"
+    }
+    url = "/api/"
+    response = client.get(url, data=json.dumps(data), headers=headers)
+    assert response.content_type == mimetype
     assert response.status_code == 200
+
+def test_get_empty(client):
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype,
+        "Key": "TEST_KEY"
+    }
+    data = {
+        "Data": ""
+    }
+    url = "/api/"
+    response = client.get(url, data=json.dumps(data), headers=headers)
+    assert response.content_type == mimetype
+    assert response.status_code == 204
+
+
+# POST
+def test_post_no_auth(client):
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype,
+        "Key": ""
+    }
+    data = {
+        "Data": "test"
+    }
+    url = "/api/"
+    response = client.post(url, data=json.dumps(data), headers=headers)
+    assert response.content_type == mimetype
+    assert response.status_code == 401
+
+def test_post_auth(client):
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype,
+        "Key": "TEST_KEY"
+    }
+    data = {
+        "Data": "test"
+    }
+    url = "/api/"
+    response = client.post(url, data=json.dumps(data), headers=headers)
+    assert response.content_type == mimetype
+    assert response.status_code == 200
+
+def test_post_empty(client):
+    headers = {
+        "Content-Type": mimetype,
+        "Accept": mimetype,
+        "Key": "TEST_KEY"
+    }
+    data = {
+        "Data": ""
+    }
+    url = "/api/"
+    response = client.post(url, data=json.dumps(data), headers=headers)
+    assert response.content_type == mimetype
+    assert response.status_code == 204

1d4edb9f5e741836c82937b60c940ab4372f7c25
eparisot <eparisot@student.42.fr>
2019-09-05 15:42:32 +0200

    install Signed-off-by: eparisot <eparisot@student.42.fr>

--- install.sh
+++ install.sh
@@ -1,27 +1,16 @@
 #!/bin/sh
-sudo apt-get install nginx supervisor python3-pip python3-venv
-#python3 -m venv env
-#source ./env/bin/activate
-#pip3 install -r requirements.txt
-#
-#echo "[program:detect_label]
-#directory=/home/admindetect/detect_label
-#command=/home/admindetect/detect_label/env/bin/gunicorn api:app -b localhost:8000
-#autostart=true
-#autorestart=true
-#stderr_logfile=/var/log/detect_label/detect_label.err.log
-#stdout_logfile=/var/log/detect_label/detect_label.out.log" > /etc/supervisor/conf.d
-#sudo supervisorctl reread
-#sudo service supervisor restart
-#
-#echo "server {
-#    listen       80;
-#    server_name  detectlabel.agoralogie.fr;
-#
-#    location / {
-#        proxy_pass http://127.0.0.1:8000;
-#    }
-#}" > /etc/nginx/conf.d/virtual.conf
-#
-#sudo nginx -t
-#sudo service nginx restart
+sudo apt-get install python-setuptools nginx supervisor python3-pip python3-venv -y
+
+python3 -m venv env
+source ./env/bin/activate
+
+pip3 install -r requirements.txt
+
+sudo sh -c 'echo "[program:detect_label]\ndirectory=/home/admindetect/detect_label\ncommand=/home/admindetect/env/bin/gunicorn api:app -b localhost:8000\nautostart=true\nautorestart=true\nstderr_logfile=/var/log/detect_label/detect_label.err.log\nstdout_logfile=/var/log/detect_label/detect_label.out.log" > /etc/supervisor/conf.d/detect_label.conf'
+sudo supervisorctl reread
+sudo service supervisor restart
+
+sudo sh -c 'echo "server {\n    listen       80;\n    server_name  detectlabel.agoralogie.fr;\n\n    location / {\n        proxy_pass http://127.0.0.1:8000;\n    }\n}" > /etc/nginx/conf.d/virtual.conf'
+
+sudo nginx -t
+sudo service nginx restart

41bb85ab2d4c18e24923baf2726130b0e4829b39
eparisot <eparisot@student.42.fr>
2019-09-16 18:51:05 +0200

    install fix mkdir -p pip3 version and hostname

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get install python3-dev python3-setuptools python3-pip python3-venv ngi
 # build and activate virtual env
 python3 -m venv env
 source ~/detect_label/env/bin/activate
-# app dep
+# app dependencies
 pip3 install -r requirements.txt
 # supervisor and Nginx config files
 
@@ -14,6 +14,7 @@ sudo service supervisor restart
 sudo cp virtual.conf /etc/nginx/conf.d/.
 sudo nginx -t
 sudo service nginx restart
-# results folder
-sudo mkdir /var/www/detect_label/results/
+# results/log folder
+sudo mkdir -p /var/www/detect_label/results/
 sudo chown $USER /var/www/detect_label/results/
+sudo mkdir -p /var/log/detect_label/
